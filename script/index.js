21a71cbf3e6fb3d0ac274489439669dbef9abb8d
David R <99845204+NORtacus@users.noreply.github.com>
2023-12-20 03:33:39 +0100

    Update index.js
    update with loader

--- script/index.js
+++ script/index.js
@@ -1,19 +1,22 @@
 const url = "https://cors.noroff.dev/https://rainydays.varmeopplevelser.no/wp-json/wc/store/products?per_page=14";
 const productContainer = document.querySelector(".products")
+
 async function getProducts(url){
+
     try {
     const response = await fetch(url);
+    productContainer.innerHTML = "";
     const products = await response.json();
 
     console.log(products);
 
     products.forEach(function(product){
         productContainer.innerHTML += 
-        `<div class="product"><h3><a href="products.html?id=${product.id}">${product.name}</a></h3>
-         <div class="product-image" style="background-image:url('${product.images[0].src}')"></div>
+        `<div class="product"><h3><a href="products.html?id=${product.id}">${product.name}</h3>
+         <div class="product-image" style="background-image:url('${product.images[0].src}')"></div></a>
          <div class="description">${product.description}</div>
          <div class="price"><h4>${product.price_html}</h4></div>
-         <div class="cart">${product.add_to_cart.text}&nbsp;<a href="?${product.add_to_cart.url}">
+         <div class="cart"><a href="${product.add_to_cart.url}">${product.add_to_cart.text}&nbsp;
          <img src="img/cart-arrow-down-solid.svg" class="cartimg"></a></div>
          </div>`; 
     });
